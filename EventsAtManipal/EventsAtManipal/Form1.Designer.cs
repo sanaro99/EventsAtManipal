291c540e9491b73eb4380de1cfac3e923b3bf113
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-04-16 01:17:56 +0530

    Add login page

--- EventsAtManipal/EventsAtManipal/Form1.Designer.cs
+++ EventsAtManipal/EventsAtManipal/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 223);
+            this.label1.MinimumSize = new System.Drawing.Size(500, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "            ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(185, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Events At Manipal";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTextBox.ForeColor = System.Drawing.Color.Gray;
+            this.userTextBox.Location = new System.Drawing.Point(112, 239);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(131, 20);
+            this.userTextBox.TabIndex = 3;
+            this.userTextBox.Text = "Username";
+            this.userTextBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.userTextBox_MouseClick);
+            this.userTextBox.TextChanged += new System.EventHandler(this.userTextBox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(282, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Password";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CC Members",
+            "Participant",
+            "Event Heads",
+            "Judges"});
+            this.comboBox1.Location = new System.Drawing.Point(443, 238);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "User Type";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(590, 223);
+            this.label3.MinimumSize = new System.Drawing.Size(100, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 50);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "            ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(614, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "LOGIN";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EventsAtManipal.Properties.Resources.mit;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.MinimumSize = new System.Drawing.Size(500, 100);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- EventsAtManipal/EventsAtManipal/Form1.cs
+++ EventsAtManipal/EventsAtManipal/Form1.cs
@@ -15,6 +15,60 @@ namespace EventsAtManipal
         public Form1()
         {
             InitializeComponent();
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label4.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new Font("Calibri ", 40);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            String user = userTextBox.Text;
+            String pass = textBox1.Text;
+            String table="";
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0: table = "CCMEMBERS";
+                    break;
+                case 1: table = "PARTICIPANTS";
+                    break;
+                case 2: table = "EVENT_HEADS";
+                    break;
+                case 3: table = "JUDGES";
+                    break;
+            }
+            String cmd = "select password from " + table + " where username=" + user;
+            MessageBox.Show(cmd);
+        }
+
+        private void userTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void userTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            userTextBox.Clear();
+            userTextBox.Font = new Font("Microsoft Sans Serif", 10);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Font = new Font("Microsoft Sans Serif", 10);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
