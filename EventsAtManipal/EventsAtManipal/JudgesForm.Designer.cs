11ef2a4623f628fb782ec164b49d095db590eb97
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-04-16 08:30:55 +0530

    Add judge functions

--- EventsAtManipal/EventsAtManipal/JudgesForm.Designer.cs
+++ EventsAtManipal/EventsAtManipal/JudgesForm.Designer.cs
@@ -33,8 +33,13 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -47,6 +52,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(536, 387);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDoubleClick);
             // 
             // label1
             // 
@@ -86,12 +92,59 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(154, 167);
+            this.button1.Location = new System.Drawing.Point(154, 314);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 37);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Next Round";
+            this.button1.Text = "Assign";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Event Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Winner";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 14);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Runner Up";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(154, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(154, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 11;
             // 
             // button2
             // 
@@ -101,30 +154,42 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(25, 418);
+            this.button2.Location = new System.Drawing.Point(291, 177);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 36);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button1";
+            this.button2.Size = new System.Drawing.Size(43, 37);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Set";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label2
+            // button3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 130);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 14);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Round Number";
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(291, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
             // 
             // JudgesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(906, 523);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
@@ -146,7 +211,12 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- EventsAtManipal/EventsAtManipal/JudgesForm.cs
+++ EventsAtManipal/EventsAtManipal/JudgesForm.cs
@@ -18,13 +18,15 @@ namespace EventsAtManipal
         {
             InitializeComponent();
             populateTable();
+            populateComboBox();
         }
+        String part;
 
         public void populateTable()
         {
             dbc.DB_Connect();
             dbc.cmd = new OracleCommand();
-            dbc.cmd.CommandText = "select * from ccmembers";
+            dbc.cmd.CommandText = "select * from participant";
             dbc.cmd.CommandType = CommandType.Text;
             dbc.ds = new DataSet();
             dbc.da = new OracleDataAdapter(dbc.cmd.CommandText, dbc.conn);
@@ -35,6 +37,64 @@ namespace EventsAtManipal
             dataGridView1.DataSource = dbc.ds;
             dataGridView1.DataMember = "Tbl_ccmembers";
             dbc.conn.Close();
+        }
+
+        private void populateComboBox()
+        {
+            dbc.DB_Connect();
+            dbc.cmd = new OracleCommand();
+            dbc.cmd.CommandText = " select event_name from event";
+            dbc.cmd.CommandType = CommandType.Text;
+            dbc.ds = new DataSet();
+            dbc.da = new OracleDataAdapter(dbc.cmd.CommandText, dbc.conn);
+            dbc.da.Fill(dbc.ds, "Tbl_volunteers");
+            dbc.dt = dbc.ds.Tables["Tbl_volunteers"];
+            int t = dbc.dt.Rows.Count;
+            dbc.dr = dbc.dt.Rows[0];
+            comboBox1.DataSource = dbc.dt.DefaultView;
+            comboBox1.DisplayMember = "EVENT_NAME";
+            dbc.conn.Close();
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            this.part = dataGridView1[0, e.RowIndex].Value.ToString();
+            //if (flag == 0)
+            //{
+            //    this.label5.Text = dataGridView1[0, e.RowIndex].Value.ToString();
+            //    flag++;
+            //}
+            //if (flag == 1)
+            //{
+            //    this.label6.Text = dataGridView1[0, e.RowIndex].Value.ToString();
+            //    flag--;
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.label5.Text = this.part;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label6.Text = this.part;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DatabaseConnection dbc = new DatabaseConnection();
+            dbc.DB_Connect();
+            int id1 = int.Parse(this.label5.Text);
+            int id2 = int.Parse(this.label6.Text);
+            dbc.cmd = new OracleCommand();
+            dbc.cmd.Connection = dbc.conn;
+            dbc.cmd.CommandText = "insert into WINNERS values('DOTA 2',"+id1+","+id2+",300,200)";
+            dbc.cmd.CommandType = CommandType.Text;
+            //dbc.cmd.ExecuteNonQuery();
+            MessageBox.Show("Inserted!");
+            dbc.conn.Close();
+
         }
     }
         //select team_id from team t, judge j where t.event_name = j.event_name;
